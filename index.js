af319e8b3bcacc3685af4ad1d807d4cba0d78625
Emmanuel Gitau <gmannu2055@gmail.com>
2025-03-05 11:13:47 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+{
+ const num1= 31;
+ const num2= 2;
+ let multiply = (num1 * num2);
+console.log(multiply);
+}
 
-// Write your code here
+
+{
+let random = Math.round (Math.random()*10);
+console.log(random);
+}
+
+
+{
+ let num3= 19
+ let num4= 5 ;
+ let  mod = (num3 % num4);
+console.log(mod);
+}
+
+{
+ let num5= 2;
+ let num6= 20;
+ let num7= 10;
+ let max = Math.max(num5,num6,num7);
+console.log(max);
+}
